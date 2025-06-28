6891fe4e65ff104914399bd6273f6bc2d2bb052f
nidhiambatkar026 <nidhiambatkar@gmail.com>
2025-06-28 12:48:40 +0530

    next code file added

---
+++ multiples.java
@@ -0,0 +1,10 @@
+public class multiples {
+    public static void main(String[] args) {
+        int n =40;
+         boolean[] primes = new boolean[n+1];
+         sieve(n,primes);
+    }
+    static boolean sieve(int num , boolean[] primes){
+         
+    }
+}
--- prime.java
+++ prime.java
@@ -13,3 +13,5 @@ public class prime {
         return true;
     }
 }
+
+
