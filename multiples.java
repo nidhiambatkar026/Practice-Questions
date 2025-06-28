6b9d0e1973e2614d1e89a4d6efe7086aea808191
nidhiambatkar026 <nidhiambatkar@gmail.com>
2025-06-28 13:10:03 +0530

    logic added

--- multiples.java
+++ multiples.java
@@ -1,10 +1,21 @@
 public class multiples {
     public static void main(String[] args) {
-        int n =40;
+        int n =100;
          boolean[] primes = new boolean[n+1];
          sieve(n,primes);
     }
-    static boolean sieve(int num , boolean[] primes){
-         
+    static void sieve(int num , boolean[] primes){
+         for(int i=2; i*i<=num;i++){
+            if(!primes[i]){
+                for(int j=i*2;j<=num;j=j+i){
+                   primes[j]=true;
+                }
+            }
+         }
+         for(int i=2;i<=num;i++){
+            if(!primes[i]){
+                System.out.print(i+" ");
+            }
+         }
     }
 }
