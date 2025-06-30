7e6579fc8100d4fb4c2979d8fd812f8fa6c811b3
nidhiambatkar026 <nidhiambatkar@gmail.com>
2025-06-30 13:27:51 +0530

    rev logic added

---
+++ reverse.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+class StringReversal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string to reverse: ");
+        String input = scanner.nextLine();
+        String reversed = reverseString(input);
+        System.out.println("Reversed string: " + reversed);
+    }
+
+    public static String reverseString(String str) {
+        char[] chars = str.toCharArray();
+        int n = chars.length;
+
+        //step 1: Reverse the entire String
+        reverse(chars, 0, n - 1);
+
+        //step 2: Reverse each word
+        int Start = 0 ;
+        for(int end =0; end < n; end++){
+            if(chars[end] == ' '){
+               reverse(chars, Start, end-1);
+               Start = end+1;
+            }
+        }
+
+        return new String(chars); 
+        
+    }
+
+    static void reverse(char[] arr, int i, int j) {
+        while(i<j){
+            char temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+
+            i++;
+            j--;
+
+        }
+    }
+}
--- rotate.java
+++ rotate.java
@@ -23,8 +23,38 @@ public class rotate {
             System.out.println();
         }
         rotate(Matrix);
+        //print Rotated Matrix
+        System.out.println("Rotated Matrix: ");
+        for(int[] row :Matrix){
+            for(int val : row){
+                System.out.print(val+" ");
+            }
+            System.out.println();
+        }
     }
     static void rotate(int [][]Matrix){
-        
+        int n=Matrix.length;
+       
+        //Transpose the matrix
+        for(int i=0;i<n;i++){
+            //swap elements at (i,j) & (j,i)
+            for(int j=1;j<n;j++){
+               int temp = Matrix[i][j];
+               Matrix[i][j] = Matrix[j][i];
+               Matrix[j][i] = temp;
+            }
+        }
+        for(int i=0; i<n; i++){
+            int Start = 0;
+            int End = n-1;
+
+            while(Start < End){
+                int temp = Matrix[i][Start];
+                Matrix[i][Start] = Matrix[i][End];
+                Matrix[i][End] = temp;
+                Start++;
+                End--;
+            }
+        }
     }
 }
