122ecea9c1e71256f69bdafe3f7079aabfd5b7e5
nidhiambatkar026 <nidhiambatkar@gmail.com>
2025-07-02 13:13:24 +0530

    Head part added

--- Linkelist/detail.java
+++ Linkelist/detail.java
@@ -21,6 +21,35 @@ class LinkedList_563{
     Node_563 head;
     void add(int data){
         Node_563 newNode = new Node_563(data);
+        //Creates a new node using the node class constructor.
+        if(head == null){
+          //if the list is empty(head==null)set the new node as the head of the list
+          head = newNode;
+        }
+        else{
+          //if list is not empty, start from the head node
+          Node_563 current = head;
+          //traverse the list until you find the last node(who's next is null).
+          while(current.next!=null){
+            current= current.next;
+            //attach the new node to the end of the list
+          }
+          current.next = newNode;
+          //by updating the next pointer of the last node.
+        }
+    }
+    //print the list(LinkedList)
+    void printList(){
+         Node_563 current=head;
+         //starts at the head of the list
+
+         while(current!=null){
+            System.out.println(current.data+" ");
+            current = current.next;
+         }
+         //traverse each node and prints its data
+         //move to the next node using , current = current.next;
+         //stops when it reaches a node that points to null.
     }
 }
 
@@ -30,5 +59,10 @@ public class detail {
         list.add(10);
         list.add(30);
         list.add(50);
+        list.add(80);
+        list.add(90);
+
+        System.out.println("Linked List");
+        list.printList();
     }
 }
